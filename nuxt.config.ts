6cde3afccaa8b89c2d1b4603fa9d49f7b596f4d1
dyedd <1176996982@qq.com>
2024-02-11 14:05:09 +0800

    feat: 主页

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
+import { siteConfig } from './site.config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    watcher: "chokidar",
+  },
+  app: {
+    head: {
+      meta: [
+        { name: 'description', content: siteConfig.description },
+        { name: 'author', content: siteConfig.author },
+        { name: 'keywords', content: siteConfig.keywords },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+        { name: 'revisit-after', content: '7 days' },
+        { name: 'msapplication-TileColor', content: '#ffffff' },
+        { charset: 'UTF-8' },
+        { 'http-equiv': 'X-UA-Compatible', 'content': 'IE=edge' },
+      ],
+      noscript: [
+        { children: 'JavaScript is required' },
+      ],
+      htmlAttrs: {
+        lang: siteConfig.lang,
+      },
+      script: [
+        { src: 'iconfont.js', type: "text/javascript" }
+      ]
+    }
+  },
   css: ['~/assets/css/main.css']
 })
