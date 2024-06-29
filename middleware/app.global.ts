3091399c01a7c5d621bcbbf004d97cc9631470c0
dyedd <1176996982@qq.com>
2024-06-29 19:22:26 +0800

    feat: 标签页标题的变换

--- middleware/app.global.ts
+++ middleware/app.global.ts
@@ -2,16 +2,13 @@ import { siteConfig } from '@/site.config'
 
 export default defineNuxtRouteMiddleware((to, _from) => {
     useHead({
-        title: to.path,
+        title: siteConfig.title,
         titleTemplate: (titleChunk) => {
-            const title = titleChunk?.replace(/^\/+/, '') || siteConfig.title
+            if (titleChunk === siteConfig.title) {
+                return titleChunk
+            }
+            const title = titleChunk + " - " +siteConfig.title
             return title
         },
-        meta: [
-            {
-                name: 'title',
-                content: to.path,
-            },
-        ],
     })
 })
